ecb84df3c71998b1f7664ce04acad1d5a9ae0e2c
ducc <spielerc@gmail.com>
2024-05-18 17:42:13 -0600

    Small AI improvements

--- Libraries/TerryController/Code/BotTerryDriver.cs
+++ Libraries/TerryController/Code/BotTerryDriver.cs
@@ -8,12 +8,16 @@ public class BotTerryDriver : TerryDriver
 {
 	[Property] public List<Vector3> CurrentPath { get; set; }
 	[Property] public float TargetReachedDistance { get; set; } = 1f;
+	[Property] public float LookLerpSpeed { get; set; } = 10f;
 
 	protected override void OnUpdate()
 	{
 		var pathDirection = GetPathDirection();
 		Controller.WishVelocity = Vector3.Forward;
-		Controller.EyeAngles = new Angles( 0, Rotation.LookAt(pathDirection).Yaw(), 0 );
+		Controller.ShouldWalk = true;
+		var targetYaw = Rotation.LookAt( pathDirection ).Yaw();
+		var currentYaw = Controller.EyeAngles.yaw.LerpTo( targetYaw, Time.Delta * LookLerpSpeed );
+		Controller.EyeAngles = new Angles( 0, currentYaw, 0 );
 	}
 
 	private Vector3 GetPathDirection()
@@ -40,5 +44,32 @@ public class BotTerryDriver : TerryDriver
 	public void NavigateTo( Vector3 targetPosition )
 	{
 		CurrentPath = Scene.NavMesh.GetSimplePath( Transform.Position, targetPosition );
+	}
+
+	protected override void DrawGizmos()
+	{
+		Gizmo.Draw.Color = Color.Green;
+
+		Gizmo.Draw.Arrow( Vector3.Zero, Controller.WishVelocity * 10f, 3, 1.5f );
+
+		if ( !Gizmo.IsSelected )
+			return;
+
+		for ( int i = 0; i < CurrentPath.Count; i++ )
+		{
+			var pathPoint = Transform.World.PointToLocal( CurrentPath[i] );
+			Gizmo.Draw.Color = Color.Green;
+			Gizmo.Draw.LineSphere( pathPoint, 3f );
+			Gizmo.Draw.Color = Color.Green * 0.9f;
+			if ( i == 0 )
+			{
+				Gizmo.Draw.Line( pathPoint, Vector3.Zero );
+			}
+			else
+			{
+				var previousPoint = Transform.World.PointToLocal( CurrentPath[i - 1] );
+				Gizmo.Draw.Line( pathPoint, previousPoint );
+			}
+		}
 	}
 }
